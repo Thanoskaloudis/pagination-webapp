154a3566c9ed994a4a074177ff7b05b1c07f3e09
Thanoskaloudis <8anos.kal@gmail.com>
2022-12-10 23:05:22 +0000

    write tests for Item components

--- src/components/Item/Item.tsx
+++ src/components/Item/Item.tsx
@@ -7,16 +7,16 @@ export const Item = (props: IBook) => {
  return(
   <Card sx={{ minWidth: 275, maxWidth: 300, minHeight: 200 }}>
     <CardContent>
-      <Typography sx={{ fontSize: 14 }} variant="h4" gutterBottom>
+      <Typography sx={{ fontSize: 14 }} variant="h4" data-testid="title" gutterBottom>
         {props.book_title}
       </Typography>
-      <Typography color="text.primary" component="div">
+      <Typography color="text.primary" data-testid="author" component="div">
         {props.book_author}
       </Typography>
-      <Typography color="text.secondary">
+      <Typography color="text.secondary" data-testid="publication-info">
       {props.book_publication_city}, {props.book_publication_country} {props.book_publication_year}
       </Typography>
-      <Typography sx={{ mb: 1, fontSize: 12, fontStyle: 'italic'}} color="text.secondary" component="span">
+      <Typography sx={{ mb: 1, fontSize: 12, fontStyle: 'italic'}} color="text.secondary" component="span" data-testid="pages">
         Pages: {props.book_pages}
       </Typography>
     </CardContent>
--- src/components/Item/Items.spec.tsx
+++ src/components/Item/Items.spec.tsx
@@ -2,12 +2,37 @@ import { render,  screen } from '@testing-library/react';
 import { mockBook, mockData } from '../../utils/mocks';
 import { Item } from './Item';
 import { testableComponent } from '../../utils/utilsTest';
-import { IBook } from '../../utils/models';
 
 describe('Item Component', () => {
-  it('Should load the correct number of list items', () => {
+  it('Should load the card with the correct book title', () => {
     render(testableComponent(<Item {...mockBook}/> , mockData));
 
+    const tittle: HTMLElement = screen.getByTestId("title");
     
+    expect(tittle.textContent).toEqual(mockData.booklist.books[1].book_title);
+  });
+
+  it('Should load the card with the correct book author', () => {
+    render(testableComponent(<Item {...mockBook}/> , mockData));
+
+    const tittle: HTMLElement = screen.getByTestId("author");
+    
+    expect(tittle.textContent).toEqual(mockData.booklist.books[1].book_author[0]);
+  });
+
+  it('Should load the card with the correct publication info(city, country, year)', () => {
+    render(testableComponent(<Item {...mockBook}/> , mockData));
+
+    const tittle: HTMLElement = screen.getByTestId("publication-info");
+    
+    expect(tittle.textContent).toEqual(`${mockData.booklist.books[1].book_publication_city}, ${mockData.booklist.books[1].book_publication_country} ${mockData.booklist.books[1].book_publication_year}`);
+  });
+
+  it('Should load the card with the correct number of pages', () => {
+    render(testableComponent(<Item {...mockBook}/> , mockData));
+
+    const tittle: HTMLElement = screen.getByTestId("pages");
+    
+    expect(tittle.textContent).toEqual(`Pages: ${mockData.booklist.books[1].book_pages}`);
   });
 });
